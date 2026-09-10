01fec5ce5d014119e878478f7ae6ffc6eb9efd49
newly.dev <api@newly.dev>
2026-09-10 16:47:05 +0000

    fix: Remove React.lazy for GameCanvasInner

--- components/GameCanvas.tsx
+++ components/GameCanvas.tsx
@@ -1,15 +1,8 @@
-import React, { Suspense } from 'react';
-import { View } from 'react-native';
-import type { GameCanvasProps } from './GameCanvasInner';
+import React from 'react';
+import { GameCanvasInner, type GameCanvasProps } from './GameCanvasInner';
 
 export type { GameCanvasProps };
 
-const LazyGameCanvasInner = React.lazy(() => import('./GameCanvasInner'));
-
 export function GameCanvas(props: GameCanvasProps) {
-  return (
-    <Suspense fallback={<View style={{ width: props.width, height: props.height, backgroundColor: '#0A0E1A' }} />}>
-      <LazyGameCanvasInner {...props} />
-    </Suspense>
-  );
+  return <GameCanvasInner {...props} />;
 }
